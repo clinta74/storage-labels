456b78958f2ff18495fc140b91a056312f251db6
Clinton Andrews <clinta74@live.com>
2025-01-14 22:55:48 -0500

    add k6 test

---
+++ storage-labels-k6/src/script.ts
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import { Rate } from 'k6/metrics';
+
+export const errorRate = new Rate('errors');
+
+export default function () {
+  const url = 'http://localhost:5166/api/test';
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  check(http.get(url, params), {
+    'status is 200': (r) => r.status == 200,
+  }) || errorRate.add(1);
+}
